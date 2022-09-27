82e8763aa80a179b549c1a4f9dcb480b833b62b3
Mekdlsdl <114468322+Mekdlsdl@users.noreply.github.com>
2022-09-27 17:18:40 +0900

    Add files via upload

---
+++ 2_practice.py
@@ -0,0 +1,54 @@
+# 2.1
+
+for i in range(1,10):
+    print(6 * i)
+
+
+
+# 2.2
+
+i = 1
+
+while True:
+    if i < 10 :
+        print(6 * i)
+
+    i += 1
+
+
+
+# 2.3
+
+for i in range(9,0,-1):
+    print(6 * i)
+
+
+
+# 2.4
+
+c = int(input())
+f = 32 + 180 / 100 * c
+print(f)
+
+
+
+# 2.5
+
+def c_to_f(c):
+    f = 32 + 180 / 100 * c
+    print(f)
+
+
+
+# 2.6
+
+a = [1, 2, 3, 4]
+
+for i in range(len(a)-1, -1, -1):
+    print(a[i], end = ' ')
+
+
+
+# 2.7
+
+    
