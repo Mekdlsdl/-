fd9ad93518a6e062b30f8769db42b163f6acdec8
Mekdlsdl <114468322+Mekdlsdl@users.noreply.github.com>
2022-10-05 13:42:49 +0900

    Add files via upload

---
+++ chapter 3/3_practice.py
@@ -0,0 +1,70 @@
+#3.2
+
+lst = []
+
+lst.insert(0,10)        #[10]
+lst.insert(1,20)        #[10,20]
+lst.insert(0,30)        #[30,10,20]
+lst.insert(2,40)        #[30,10,40,20]
+lst.insert(len(lst),50) #[30,10,40,20,50]
+
+print(lst)
+
+
+
+#3.3
+
+lst.insert(1,60)        #[30,60,10,40,20,50]
+lst[2]=70               #[30,60,70,40,20,50]
+lst.pop(2)              #[30,60,40,20,50]
+
+print(lst)
+
+
+
+#3.5
+
+def Max_list(lst):
+    max_lst = max(lst)
+    return max_lst
+
+print("최대 =",Max_list(lst))
+
+
+
+#3.6
+
+def Min_max_list(lst):
+    max_lst = max(lst)
+    min_lst = min(lst)
+    ans_tup = (max_lst, min_lst)
+    return ans_tup
+
+print("(최대, 최소) =",Min_max_list(lst))
+
+
+
+#3.7
+
+def Duplication_list(lst1, lst2):
+    count = 0
+    for i in lst1:
+        for j in lst2:
+            if i == j:
+                count += 1
+            else:
+                count += 0
+    if count == 0:
+        return False
+    else:
+        return True
+
+lst2 = [10,20,30]
+lst3 = [10,70,80,90]
+
+print("동일한 항목 있음",Duplication_list(lst,lst2))
+print("동일한 항목 없음",Duplication_list(lst,lst3))
+
+
+
+            
