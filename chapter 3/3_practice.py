ccc88974536b042741b876b81ee0c1fdbcd15402
Mekdlsdl <114468322+Mekdlsdl@users.noreply.github.com>
2022-10-06 23:01:00 +0900

    Update 3_practice.py

--- chapter 3/3_practice.py
+++ chapter 3/3_practice.py
@@ -67,4 +67,38 @@ print("동일한 항목 없음",duplicationList(lst,lst3))
 
 
 
-            
+#3.8
+ def mergeSortList(lst1, lst2):
+    i = 0
+    j = 0
+    k = 0
+    A = []
+
+    while i < len(lst1) and j < len(lst2):
+        if lst1[i] < lst2[j]:
+            A.insert(k, lst1[i])
+            i += 1
+        else:
+            A.insert(k, lst2[j])
+            j += 1
+        k += 1
+
+    while i < len(lst1):
+        A.insert(k, lst1[i])
+        i += 1
+        k += 1
+
+    while j < len(lst2):
+        A.insert(k, lst2[j])
+        j += 1
+        k += 1
+
+    return A
+
+
+
+lst4 = [10,20,50]
+lst5 = [10,30,40]
+
+print(mergeSortList(lst4,lst5))
+           
